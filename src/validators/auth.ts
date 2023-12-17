e3d89432a376b5aa8950a4e49d7f34549e046106
Angela <yushinwu.angela@gmail.com>
2023-12-17 23:02:29 +0800

    Test branch

--- src/validators/auth.ts
+++ src/validators/auth.ts
@@ -2,8 +2,5 @@ import { z } from "zod";
 
 export const authSchema = z.object({
   username: z.string(),
-  // email: z.string().email(),
-  // Passwords must be at least 8 characters long.
-  // password: z.string().min(8),
   password: z.string(),
 });
