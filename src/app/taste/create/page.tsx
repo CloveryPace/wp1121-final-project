2296a19fa83e117d239d7d319681db26b5aa66b7
asheseux16 <asheseux16@gmail.com>
2023-12-27 20:08:41 +0800

    frontend

--- src/app/taste/create/page.tsx
+++ src/app/taste/create/page.tsx
@@ -22,11 +22,12 @@ type FormValues = {
   taste_info: {
     taste_name: string;
     taste_count: number;
-    taste_category: string;
+    // taste_category: string;
     taste_photo: string;  // url
   }[];
-  taste_place: string;
+  taste_category: string;
   taste_time: string;  // data
+  taste_place: string;
 }
 
 function CreatePage() {
@@ -35,11 +36,12 @@ function CreatePage() {
       taste_info: [{
         taste_name: "",
         taste_count: 1,
-        taste_category: "",
+        // taste_category: "",
         taste_photo: "",
       }],
-      taste_place: "",
+      taste_category: "",
       taste_time: "",
+      taste_place: "",
     }
   })
 
@@ -80,11 +82,9 @@ function CreatePage() {
                 {...register(`taste_info.${index}.taste_count` as const)}
               />
             </div>
-            <div className="w-20" key={field.id}>
+            {/* <div className="w-20" key={field.id}>
               <Label htmlFor="taste-categoy" className="text-base font-semibold">標籤</Label>
-              {/* <select value = {val.category} id="taste-category" className="h-10 w-full px-2 border border-black text-base rounded-md focus:border-theme-green block"> */}
               <select {...register(`taste_info.${index}.taste_category` as const)} id="taste-category" className="h-10 w-full px-2 border border-black text-base rounded-md focus:border-theme-green block">
-                {/* <option value="none" selected disabled hidden>----</option> */}
                 <option value="taiwnese">臺式</option>
                 <option value="chinese">中式</option>
                 <option value="western">西式</option>
@@ -94,7 +94,7 @@ function CreatePage() {
                 <option value="drinks">飲料</option>
                 <option value="desserts">甜點</option>
               </select>
-            </div>
+            </div> */}
             <div className="w-20" key={field.id}>
               <Label className="text-base font-semibold">照片</Label>
               <label htmlFor="taste-photo">
@@ -106,7 +106,7 @@ function CreatePage() {
               </label>
             </div>
             {index === 0 && (
-              <button className="pt-5 ml-4" onClick={() => append({taste_name: "", taste_count: 1, taste_category: "", taste_photo: ""})}>
+              <button className="pt-5 ml-4" onClick={() => append({taste_name: "", taste_count: 1, taste_photo: ""})}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg>
@@ -123,19 +123,36 @@ function CreatePage() {
           </div>
         ))}
 
+        <div className="flex space-x-6">
+          <div className="w-20">
+            <Label htmlFor="taste-categoy" className="text-base font-semibold">標籤</Label>
+            {/* <select value = {val.category} id="taste-category" className="h-10 w-full px-2 border border-black text-base rounded-md focus:border-theme-green block"> */}
+            <select id="taste-category" className="h-10 w-full px-2 border border-black text-base rounded-md focus:border-theme-green block">
+              <option value="none" selected disabled hidden>----</option>
+              <option value="taiwnese">臺式</option>
+              <option value="chinese">中式</option>
+              <option value="western">西式</option>
+              <option value="japanese">日式</option>
+              <option value="korean">韓式</option>
+              <option value="breakfast">早餐</option>
+              <option value="drinks">飲料</option>
+              <option value="desserts">甜點</option>
+            </select>
+          </div>
+          <div className="w-3/5">
+            <Label htmlFor="taste-time" className="text-base font-semibold">最後取餐時間</Label>
+            <Input
+              type="text"
+              id="taste-time"
+              className='flex h-10 w-4/5 rounded-md border border-black text-base'
+            />
+          </div>
+        </div>
         <div>
           <Label htmlFor="taste-place" className="text-base font-semibold">取餐地點</Label>
           <Input
             type="text"
             id="taste-place"
-            className='flex h-10 w-4/5 rounded-md border border-black text-base'
-          />
-        </div>
-        <div>
-          <Label htmlFor="taste-time" className="text-base font-semibold">最後取餐時間</Label>
-          <Input
-            type="text"
-            id="taste-time"
             className='flex h-10 w-4/5 rounded-md border border-black text-base'
           />
         </div>
