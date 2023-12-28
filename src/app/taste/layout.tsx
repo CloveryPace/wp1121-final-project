8ec120b05163921b43971095dcdc972e122727e7
asheseux16 <asheseux16@gmail.com>
2023-12-28 23:35:28 +0800

    ui fix

--- src/app/taste/layout.tsx
+++ src/app/taste/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from "./_components/Navbar";
 
 function TasteLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="h-screen w-full">
+    <main className="h-screen w-full overflow-hidden">
       <Navbar />
       <div className="no-scrollbar w-full overflow-y-scroll">{children}</div>
     </main>
--- src/app/taste/page.tsx
+++ src/app/taste/page.tsx
@@ -1,5 +1,5 @@
-// import { BiError } from "react-icons/bi";
 import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 import getFood from "../actions/getFood";
@@ -19,41 +19,72 @@ async function TastePage() {
   const food = await getFood();
 
   return (
-    <div className="mx-12 flex h-screen w-full items-center justify-center space-x-4">
-      {/* <div className="flex flex-col items-center justify-center"> */}
-      {/* <p className="text-lg font-semibold text-slate-700">
-          {JSON.stringify(session)}
-        </p>
-        <p className="text-lg font-semibold text-slate-700">
-          {JSON.stringify(food)}
-        </p> */}
+    <div className="no-scrollbar my-32 h-96 w-full space-y-6 overflow-y-scroll">
+      <div className="no-scrollbar flex space-x-4 overflow-x-scroll px-12">
+        {food?.map((item, i) => {
+          if (item.count > 0) {
+            return (
+              <Card className="h-64 w-96 cursor-pointer select-none" key={i}>
+                <Link href={`/taste/${item.food_id}`}>
+                  <CardHeader className="max-h-32">
+                    <Image
+                      src="/potato-salad.svg"
+                      alt="food"
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                      // fill
+                      // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                      style={{ width: "100%", height: "40%" }}
+                      priority
+                    />
+                  </CardHeader>
+                  <CardContent className="no-scrollbar h-2/5 overflow-y-scroll">
+                    <CardTitle>{item.name}</CardTitle>
+                    <div>數量：{item.count}</div>
+                    <div>時間：{item.time}</div>
+                    <div>地點：{item.place}</div>
+                  </CardContent>
+                </Link>
+              </Card>
+            );
+          }
+        })}
+      </div>
 
-      {food?.map((item, i) => {
-        return (
-          <Card className="h-1/2 w-full" key={i}>
-            <CardHeader>
-              <Image
-                src="/potato-salad.svg"
-                alt="food"
-                width={0}
-                height={0}
-                sizes="100vw"
-                style={{ width: "100%", height: "40%" }}
-                priority
-              />
-            </CardHeader>
-            <CardContent>
-              <CardTitle>{item.name}</CardTitle>
-              <div>數量：{item.count}</div>
-              <div>時間：{item.time}</div>
-              <div>地點：{item.place}</div>
-            </CardContent>
-          </Card>
-        );
-      })}
-
+      <div className="flex space-x-4 px-12">
+        {food?.map((item, i) => {
+          if (item.count > 0) {
+            return (
+              <Card className="h-64 w-48 cursor-pointer select-none" key={i}>
+                <Link href={`/taste/${item.food_id}`}>
+                  <CardHeader className="max-h-32">
+                    <Image
+                      src="/potato-salad.svg"
+                      alt="food"
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                      // fill
+                      // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                      style={{ width: "100%", height: "40%" }}
+                      priority
+                    />
+                  </CardHeader>
+                  <CardContent className="no-scrollbar h-2/5 overflow-y-scroll">
+                    <CardTitle>{item.name}</CardTitle>
+                    <div>數量：{item.count}</div>
+                    <div>時間：{item.time}</div>
+                    <div>地點：{item.place}</div>
+                  </CardContent>
+                </Link>
+              </Card>
+            );
+          }
+        })}
+      </div>
       {/* </div> */}
-      <section className="flex w-full flex-col pt-3"></section>
+      {/* <section className="flex w-full flex-col pt-3"></section> */}
     </div>
   );
 }
